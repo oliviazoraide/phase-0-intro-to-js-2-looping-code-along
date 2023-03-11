d8b295b5b03454162da887e02ac1ea4aa5f5dbf4
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-03-11 12:26:49 -0800

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event) {
+    let message = []
+    for ( let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
