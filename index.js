e66a4e8a285070267e36ebc0fe6735e15fd4482a
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-04-05 00:56:31 -0700

    completed loop lab

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const names = ["Charlie", "Samip", "Ali"];
-const event = "birthday";
-
-function writeCards(names, event) {
-    let message = []
-    for ( let i = 0; i < names.length; i++) {
-        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(name, event) {
+    let message = [];
+    for(let i = [0]; i < names.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
     }
     return message;
 }
 
-function countDown(number) {
-    while (number >= 0) {
-        console.log(number--)
+function countDown() {
+    let n = 10;
+    while(n >= 0) {
+        console.log(n);
+        n--;
     }
 }
